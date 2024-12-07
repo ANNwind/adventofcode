823962c3a7f5b0173522db0550a81afa4da923f8
Anne <anne.wind@unive.nl>
2024-12-07 20:06:49 +0100

    part 1 ass 3

--- 2024/assThree.js
+++ 2024/assThree.js
@@ -1,2 +1,15 @@
-input = document.querySelector("body > pre").innerText;
-input.match(/mul\(\d{1,3},\d{1,3}\)/g)
+var input = document.querySelector("body > pre").innerText;
+var validMuls = input.match(/mul\(\d{1,3},\d{1,3}\)/g)
+
+function exeCuteMul(str) {
+    return str.match(/\d{1,3},\d{1,3}/g).join().split(",").map(Number).reduce((acc, curr) => {return acc * curr})    
+   }
+debugger;
+exeCutedMuls = [];
+validMuls.forEach((mul) => {
+    exeCutedMuls.push(exeCuteMul(mul))
+})
+
+exeCutedMuls.reduce((acc, curr) => {return acc + curr},0)
+
+// part 2
