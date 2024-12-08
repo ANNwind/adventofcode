c3f8aa1acd70fc5d5e217ee421aadd3016f9509b
Anne <anne.wind@unive.nl>
2024-12-08 19:57:20 +0100

    Fixed ass 3

--- 2024/assThree.js
+++ 2024/assThree.js
@@ -13,24 +13,29 @@ validMuls.forEach((mul) => {
 exeCutedMuls.reduce((acc, curr) => {return acc + curr},0)
 
 // part 2
-
+var input = document.querySelector("body > pre").innerText;
 var newInput = input.split(/(do\(.*?\)|don't\(.*?\))/g);
-// var keepOnRocking = true
+var usedMuls = [];
+var ansArr = [];
 
-for (i=0; i < newInput; i++) {
-    if (i !== "don't()") {
-        console.log(i)
-    } else if (i === "don't()"){
-        continue
+function exeCuteMul(str, resArr) {
+        var muls = str.match(/mul\(\d{1,3},\d{1,3}\)/g);
+        var executedMuls = muls.map((str) => str.match(/\d{1,3},\d{1,3}/g).join().split(",").map(Number).reduce((acc, curr) => {return acc * curr}))
+        resArr.push(executedMuls);   
+   }
+
+debugger;
+skipIteration = false
+for (i=0; i < newInput.length; i++) {
+    if(newInput[i] === "do()" && skipIteration === true) {
+        skipIteration = false
+    } else if ((newInput[i] !== "don't()" && newInput[i] !== "do()") && skipIteration === false) {
+        exeCuteMul(newInput[i], ansArr); 
+    } else if (newInput[i] === "don't()") {
+        skipIteration = true
     }
 
 }
 
-while (keepOnRocking) { // niet nodig?
-    // loop door array
-    // check of i !don't() is
-    // function die exeCuteMul aanroep en ook wegschrijft naar array
-    // else als i don't() is, continue totdat i do() is
-    // // function die exeCuteMul aanroep en ook wegschrijft naar array
-
-}
+ansArrFlattened = ansArr.flat()
+console.log("The answer is: ",ansArrFlattened.reduce((acc, curr) => {return acc + curr},0));
