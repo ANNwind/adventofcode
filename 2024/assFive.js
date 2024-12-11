80f104cc29d9c835a9cd2cd9d66f146110641a59
Anne <anne.wind@unive.nl>
2024-12-11 15:30:16 +0100

    input extraction

---
+++ 2024/assFive.js
@@ -0,0 +1,15 @@
+/**
+ * Identify which updates are already in the right order.
+ * X needs to be before Y at some point
+ * 
+ * Build object that holds X as primary and an array with all Y values
+ * 
+ * Middle page number of each correctly ordered update (row.length / 2) with modulus or something. you figure it out.
+ * 
+ * Add all middle page numbers
+ */
+
+var rawInput = document.querySelector("body > main > article > pre:nth-child(8) > code").innerText.split("\n"); // test input
+var rules = rawInput.filter((row) => row.includes("|"));
+var updates = rawInput.filter((row) => row.includes(","));
+updates
