631cee3e214e2153cfff36fd175f63aacfc0bb8a
Anne <anne.wind@unive.nl>
2024-12-11 16:03:43 +0100

    Only need to define the function that check validity and extracts middle number

--- 2024/assFive.js
+++ 2024/assFive.js
@@ -7,6 +7,8 @@
  * Middle page number of each correctly ordered update (row.length / 2) with modulus or something. you figure it out.
  * 
  * Add all middle page numbers
+ * 
+ * Get i of update, retrieve values of object, loop through values and check if they are after 75
  */
 
 var rawInput = document.querySelector("body > main > article > pre:nth-child(8) > code").innerText.split("\n"); // test input
@@ -25,3 +27,21 @@ for (i=0; i < rules.length; i++) {
     }
 }
 
+function generateAnser(num) {
+    // check the validity of the update number by comparing each next sibling (just count) if it is in the object
+    // if it is valid extract the middle number
+    // push it to global array
+    // no return statement?
+}
+
+var ansArr = [];
+// Loop through updates array
+for (i=0; i < updates.length; i++) {
+    for (num=0; num < updates[i].length; num++) {
+        if (typeof checkPrecedence[num] !== 'undefined') { // our update is in the object
+            // call Function that checks the validity of the updates
+        }
+    }
+}
+
+ansArr.reduce((acc, curr) => acc + curr)
